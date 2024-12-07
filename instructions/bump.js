b8b2f25971e6a026c865149d8d59c80e6e3eeb8d
6ayauakwlqpqpl <md.shahwaezabir21@gmail.com>
2024-12-07 11:51:49 +0600

    Update bump.js

--- instructions/bump.js
+++ instructions/bump.js
@@ -33,7 +33,7 @@ module.exports = {
 
 async function sendBumpCommand(channel) {
   try {
-    await channel.sendSlash('1154077045903593555', 'bump');
+    await channel.sendSlash('415773861486002186', 'bump');
   } catch (error) {
     console.error('Failed to send /bump command:', error);
   }
