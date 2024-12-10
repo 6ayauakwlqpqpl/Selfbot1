fa03948acaa0a6f3eddc6f1b32f49f75512e8092
6ayauakwlqpqpl <md.shahwaezabir21@gmail.com>
2024-12-10 10:29:40 +0600

    Update bump.js

--- instructions/bump.js
+++ instructions/bump.js
@@ -21,7 +21,7 @@ module.exports = {
             console.error(`Error sending /bump command:`, error);
           }
           startBumping(); 
-        }, Math.round(Math.random() * (3660000 - 3600000 + 1)) + 3600000);
+        }, Math.round(Math.random() * (7260000 - 7200000 + 1)) + 7200000);
       };
       startBumping();
       message.channel.send(`\`\`\`✔ Started sending /bump command at random intervals in this channel.\`\`\``);
@@ -33,7 +33,7 @@ module.exports = {
 
 async function sendBumpCommand(channel) {
   try {
-    await channel.sendSlash('1089476688246738985', 'bump');
+    await channel.sendSlash('302050872383242240', 'bump');
   } catch (error) {
     console.error('Failed to send /bump command:', error);
   }
